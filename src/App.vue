34b270350821ca8396b44d2c26dd4e0656b937f7
markcaf <marcocafassog@gmail.com>
2022-07-15 16:10:57 +0200

    Import of the three primary components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    
+    <Header/>
+    <Main/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
+    Main,
+    Footer,
   }
 }
 </script>
