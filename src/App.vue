84123a182140180357e77ee22978b111edaef892
markcaf <marcocafassog@gmail.com>
2022-07-15 17:24:58 +0200

    Imported the variables.scss

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,11 @@ export default {
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap.scss";
+@import "./styles/variables.scss";
 
-
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 </style>
