31421eb4fb76893cc371f4cdf1d2b146095947d8
markcaf <marcocafassog@gmail.com>
2022-07-18 23:44:38 +0200

    Created the Jumbo component with jumbotron

---
+++ src/components/Jumbo.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+
+    </section>
+  
+</template>
+
+<script>
+export default {
+    name: "Jumbo",
+
+}
+</script>
+
+<style lang="scss" scoped>
+    section{
+        height: 350px;
+        background-image: url(../assets/img/jumbotron.jpg);
+        background-size: cover;
+    }
+</style>
