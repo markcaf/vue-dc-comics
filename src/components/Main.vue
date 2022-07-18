8f8476de6620fbaca60adab3665f93138efe87c1
markcaf <marcocafassog@gmail.com>
2022-07-18 23:58:59 +0200

    Added the heading bar in position absolute

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,12 @@
 <template>
     <main>
         <div class="container">
+            <div class="row">
+                <div class="col-2 heading text-center py-2">
+                    Current Series
+                </div>
+            </div>
+
             <div class="row flex-column">
                 <div class="col-12 text-white py-5 fs-3">
                     --> Content goes here
@@ -19,9 +25,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../styles/variables.scss";
 
     main{
         background-color: black;
+        position: relative;
+
+        .heading{
+            color: white;
+            background-color: $brandColor;
+            font-size: 20px;
+            text-transform: uppercase;
+            font-weight: bold;
+            font-stretch: ultra-condensed;
+            position: absolute;
+            top: -25px;
+        }
     }
 
 </style>
