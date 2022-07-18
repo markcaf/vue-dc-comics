ff3ee5e408b2e0f7e16a5851a920dcb2ed9e6266
markcaf <marcocafassog@gmail.com>
2022-07-19 00:43:20 +0200

    Added static markup of comic cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,9 +7,71 @@
                 </div>
             </div>
 
-            <div class="row flex-column">
-                <div class="col-12 text-white py-5 fs-3">
-                    --> Content goes here
+            <div class="row">
+                <div class="col-12 py-5 comics-container d-flex flex-wrap">
+
+                    <div class="comic mb-5">
+                        <div class="image-container">
+                            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                        </div>
+                        <div class="title text-white text-uppercase mt-3">
+                            Action Comics
+                        </div>
+                    </div>
+
+                    <div class="comic mb-5">
+                        <div class="image-container">
+                            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                        </div>
+                        <div class="title text-white text-uppercase mt-3">
+                            Action Comics
+                        </div>
+                    </div>
+
+                    <div class="comic mb-5">
+                        <div class="image-container">
+                            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                        </div>
+                        <div class="title text-white text-uppercase mt-3">
+                            Action Comics
+                        </div>
+                    </div>
+
+                    <div class="comic mb-5">
+                        <div class="image-container">
+                            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                        </div>
+                        <div class="title text-white text-uppercase mt-3">
+                            Action Comics
+                        </div>
+                    </div>
+
+                    <div class="comic mb-5">
+                        <div class="image-container">
+                            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                        </div>
+                        <div class="title text-white text-uppercase mt-3">
+                            Action Comics
+                        </div>
+                    </div>
+                    
+                    <div class="comic mb-5">
+                        <div class="image-container">
+                            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                        </div>
+                        <div class="title text-white text-uppercase mt-3">
+                            Action Comics
+                        </div>
+                    </div>
+
+                    <div class="comic mb-5">
+                        <div class="image-container">
+                            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                        </div>
+                        <div class="title text-white text-uppercase mt-3">
+                            Action Comics
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -21,6 +83,87 @@
 <script>
 export default {
     name: "Main",
+
+    data: function () {
+        return {
+
+            comicsCards: [
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+            ]
+    
+        }
+    }
 }
 </script>
 
@@ -28,7 +171,7 @@ export default {
 @import "../styles/variables.scss";
 
     main{
-        background-color: black;
+        background-color: #1c1c1c;
         position: relative;
 
         .heading{
@@ -41,6 +184,31 @@ export default {
             position: absolute;
             top: -25px;
         }
+
+        .comics-container{
+            margin: auto;
+
+            .comic{
+            width: calc(100% / 6);
+
+                .image-container{
+                    height: 192px;
+                    width: 192px;
+
+                    img.comic-cover{
+                        height: 100%;
+                        width: 100%;
+                        object-fit: cover;
+                        object-position: top;
+                    }
+                }
+
+                .title{
+                    font-size: 14px;
+                }
+            }
+        }
+        
     }
 
 </style>
